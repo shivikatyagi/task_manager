16e97fcc4a82dacbe54dbcd52b547f3570a7f7e8
Shivika Tyagi <iamshivikatyagi@gmail.com>
2022-01-16 20:28:29 +0530

    monoose setup and data validation & sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,26 +1,23 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
 
-const me = new User({
-    name: 'Shivika',
-    age: 19
-})
+// const me = new User({
+//     name: 'Shivika',
+//     email: 'shivikatyagi@gmail.com',
+//     password: '1234567',
+//     age: 19
+// })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validatea(value){
+            if(validator.value.includes("password")){
+                throw new Error('password cannot be "password"!!')
+            }
+        }
+
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    }
+})
+
+module.exports = User
